9098c54637eaf5979199e5c5cb76c19ff20f6954
C Anthony Risinger <anthony.risinger@corvisa.com>
2015-10-12 12:22:59 -0500

    remove broken things

--- retrospect/__init__.py
+++ retrospect/__init__.py
@@ -17,36 +17,38 @@ class FunctionVanished(Exception):
 class FunctionRetrospector(object):
 
     def __init__(self, function):
-        self.function_ref = weakref.ref(function, self._dead_function_cb)
-        self.function_code = function.__code__
+        self._function_ref = weakref.ref(function)
+        self._function_code = function.__code__
 
-    def _dead_function_cb(self, *args, **kwds):
-        print('>>> _dead_function_cb: {} {}'.format(args, kwds))
-
-    def emission_opcodes(self, *context):
-        return [(byteplay.LOAD_CONST, emit),
-                (byteplay.LOAD_CONST, locals),
-                (byteplay.CALL_FUNCTION, 0),
+    def _emission_opcodes(self, emit=None, context=None,
+                          globals=globals, locals=locals):
+        # TODO: Maybe use our own code here with MAKE_CLOSURE + LOAD_CLOSURE
+        # to inject a closure, binding the context to our new function, and
+        # enabling emit to then "ask" it for globals/locals/sym as/if needed.
+        return [(byteplay.LOAD_CONST, emit or _emit),
+                (byteplay.LOAD_CONST, context),
                 (byteplay.LOAD_CONST, globals),
                 (byteplay.CALL_FUNCTION, 0),
-                (byteplay.LOAD_CONST, context),
+                (byteplay.LOAD_CONST, locals),
+                (byteplay.CALL_FUNCTION, 0),
                 (byteplay.CALL_FUNCTION, 3),
                 (byteplay.POP_TOP, None)]
 
     def implement(self, **kwds):
-        function = self.function_ref()
+        function = self._function_ref()
         if function is None:
             raise FunctionVanished(self)
 
         if not kwds:
-            function.__code__ = self.function_code
+            function.__code__ = self._function_code
             return
 
-        key = str(time.time())
+        # defaults
+        kwds.setdefault('boundaries', ('start', 'finish'))
 
-        # strategies
+        # TODO: move elsewhere
         poi = dict()
-        for point in ('symbols', 'lines', 'bytecode'):
+        for point in ('symbols', 'lines', 'bytecode', 'boundaries'):
             # start with what the user sent else default empty
             interest = kwds.pop(point, None) or tuple()
 
@@ -66,68 +68,41 @@ class FunctionRetrospector(object):
 
         lineno = 0
         instructions = list()
-        editable = byteplay.Code.from_code(self.function_code)
+        editable = byteplay.Code.from_code(self._function_code)
         for opcode, oparg in editable.code:
-            # TODO: provide context
             emit_before = emit_after = False
-
-            if poi['bytecode'] is True or opcode in poi['bytecode']:
-                # TODO: need to provide top-of-stack
-                emit_before = True
             if opcode == byteplay.SetLineno:
+                if lineno == 0:
+                    if poi['boundaries'] is True or 'start' in poi['boundaries']:
+                        emit_before = True
                 if poi['lines'] is True or oparg in poi['lines']:
                     emit_before = True
-            if opcode == byteplay.RETURN_VALUE:
-                # about to leave the frame
-                emit_before = True
-            if opcode == byteplay.STORE_FAST:
+                lineno = oparg
+            elif opcode == byteplay.RETURN_VALUE:
+                if poi['boundaries'] is True or 'finish' in poi['boundaries']:
+                    # about to leave the frame
+                    emit_before = True
+            elif opcode == byteplay.STORE_FAST:
                 if poi['symbols'] is True or oparg in poi['symbols']:
                     # inspect before and after the call
                     emit_before = True
                     emit_after = True
+            elif poi['bytecode'] is True or str(opcode) in poi['bytecode']:
+                # TODO: need to provide top-of-stack
+                emit_before = True
 
+            # TODO: list of reason(s) needed for future filtering
+            reason = (lineno, opcode, oparg)
             if emit_before:
-                instructions.extend(self.emission_opcodes(None))
+                instructions.extend(self._emission_opcodes(context=reason))
             instructions.append((opcode, oparg))
             if emit_after:
-                instructions.extend(self.emission_opcodes(None))
-
-            if opcode == byteplay.SetLineno:
-                lineno = oparg
+                instructions.extend(self._emission_opcodes(context=reason))
 
         editable.code = instructions
         function.__code__ = editable.to_code()
 
 
-class Retrospector(object):
-
-    def __init__(self, fun):
-        if not hasattr(fun, '__code__'):
-            raise TypeError('function needs __code__')
-
-        self.fun = fun
-
-    def trace(self, **kwds):
-        fun = self.fun
-        key = frozenset(kwds.items()) or None
-        tracer = cache.get(key)
-        if tracer is None:
-            tracer = cache[key] = cache[None].implement(**kwds)
-
-        fun.__code__ = tracer.code
-        return self
-
-
-def retrospect(fun):
-    fun = getattr(fun, '__func__', fun)
-    rspect = getattr(fun, '__retrospect__', None)
-    if rspect:
-        return rspect
-
-    rspect = fun.__retrospect__ = Retrospector(fun)
-    return rspect
-
-
-def emit(l, g, c=None):
-    from pprint import pprint
-    pprint(l)
+def _emit(_context, _globals, _locals):
+    from pprint import pprint as pp
+    pp((_context, _locals))
